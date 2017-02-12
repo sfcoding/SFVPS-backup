dde1816e372a2d03b10c1f64a5a7cceae88cdd1c
root <root@SFvps>
2017-02-13 00:09:52 +0100

    just formatting

--- PerlEmail.pl
+++ PerlEmail.pl
@@ -3,15 +3,12 @@
 use warnings;
 use strict;
 
-
 my $to = 'andreagalloni92@gmail.com,lucarin91@gmail.com,andrea.galloni@studenti.unitn.it';
 my $from = 'no-reply@sfcoding.com';
 my $subject = '[SFvps] BACKUP TESTS FAILED';
 my $message =  
-"This message was sent to notify the System Admin.
-
-Possibly some tests about backups on MEGA have failed :(
-
+"This message was sent to notify the System Admin.\n\n
+Possibly some tests about backups on MEGA have failed :(\n\n
 Please check /var/log/backup-mega-test.log on the vps for further details.\n\n
 Cheers\n";
  
@@ -21,6 +18,7 @@ open(MAIL, "|/usr/sbin/sendmail -t");
 print MAIL "To: $to\n";
 print MAIL "From: $from\n";
 print MAIL "Subject: $subject\n\n";
+
 # Email Body
 print MAIL $message;
 
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# if cpan is too heavy for your host try using cpanm
+# depends on megatools 1.9.97
+
 sudo apt-get install libmysqlclient-dev
 sudo cpan YAML Config::Simple DBI DBD::mysql Capture::Tiny
 sudo apt-get -y install build-essential libglib2.0-dev libssl-dev \
